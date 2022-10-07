74e5a0c94cbea56f339c96e01560345d17806e95
Joshbam-joshwilly <Alanajoshua535@gmail.com>
2022-10-07 11:38:21 -0700

    temp.m

---
+++ test-dir/temp.m
@@ -0,0 +1,19 @@
+push 		2
+		push 5
+		push 			-75
+		push 8
+pall
+swap
+nop
+add
+							pall
+							pint
+							push  13
+							push 11
+							push 22
+							pint
+							pop
+							pint
+							pop	a
+
+							pall
